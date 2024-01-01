4ac036e484fb9516b372ce55bf62759bea678c4e
Werner Hahn <werner_hahn@gmx.com>
2024-01-01 12:47:10 +0100

    day 21

--- advent2023/src/main/java/com/github/musiKk/Day21.java
+++ advent2023/src/main/java/com/github/musiKk/Day21.java
@@ -5,8 +5,10 @@ import java.nio.file.Path;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Day21 {
@@ -33,7 +35,8 @@ public class Day21 {
             }
         }
 
-        part1(map, startRow, startCol);
+        // part1(map, startRow, startCol);
+        part2(map, startRow, startCol);
     }
 
     static void dumpMap(char[][] map) {
@@ -41,41 +44,47 @@ public class Day21 {
     }
 
     static void part2(char[][] map, int startRow, int startCol) {
+        int height = map.length;
+        int width = map[0].length;
+
+        Set<Coord> visited = new HashSet<>();
+
         Deque<Coord> q = new ArrayDeque<>();
         q.add(new Coord(startRow, startCol));
 
         int steps = 0;
-        int reached = 0;
+        Map<Coord, Integer> coordToMinSteps = new HashMap<>();
         while (!q.isEmpty()) {
-            System.err.println("reached after " + steps + " steps: " + reached);
-
-            if (steps == 64) break;
-
             int len = q.size();
-            Set<Coord> newSteps = new HashSet<>();
             for (int i = 0; i < len; i++) {
-                var pos = q.remove();
+                var coord = q.remove();
 
-                int r = pos.row;
-                int c = pos.col;
-                if (r < 0 || r == map.length || c < 0 || c == map[0].length) continue;
+                int row = coord.row;
+                int col = coord.col;
+                if (row < 0 || row == height || col < 0 || col == width) continue;
+                if (map[row][col] == '#') continue;
 
-                if (r > 0 && map[r - 1][c] == '.')
-                    newSteps.add(new Coord(r - 1, c));
-                if (r < map.length - 1 && map[r + 1][c] == '.')
-                    newSteps.add(new Coord(r + 1, c));
-                if (c > 0 && map[r][c - 1] == '.')
-                    newSteps.add(new Coord(r, c - 1));
-                if (c < map[0].length - 1 && map[r][c + 1] == '.')
-                    newSteps.add(new Coord(r, c + 1));
+                if (visited.contains(coord)) continue;
+                visited.add(coord);
 
+                coordToMinSteps.put(coord, steps);
+
+                q.add(new Coord(row - 1, col));
+                q.add(new Coord(row + 1, col));
+                q.add(new Coord(row, col - 1));
+                q.add(new Coord(row, col + 1));
             }
-            reached = newSteps.size();
-            q.addAll(newSteps);
             steps++;
         }
 
-        System.err.println(reached);
+        var evenCorners = coordToMinSteps.values().stream().filter(v -> v % 2 == 0 && v > 65).count();
+        var oddCorners = coordToMinSteps.values().stream().filter(v -> v % 2 == 1 && v > 65).count();
+        var evenFull = coordToMinSteps.values().stream().filter(v -> v % 2 == 0).count();
+        var oddFull = coordToMinSteps.values().stream().filter(v -> v % 2 == 1).count();
+
+        long n = 202300;
+        long result = ((n+1)*(n+1)) * oddFull + (n*n) * evenFull - (n+1) * oddCorners + n * evenCorners;
+        System.err.println(result);
     }
 
     static void part1(char[][] map, int startRow, int startCol) {
