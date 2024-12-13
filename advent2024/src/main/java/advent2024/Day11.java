2377fa2e100d18bd82200ced5ddc2f319f3878f4
Werner Hahn <werner.p.hahn@gmail.com>
2024-12-13 18:48:53 +0100

    day 11 part 2

--- advent2024/src/main/java/advent2024/Day11.java
+++ advent2024/src/main/java/advent2024/Day11.java
@@ -5,7 +5,9 @@ import java.nio.file.Path;
 import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Day11 {
@@ -17,21 +19,52 @@ public class Day11 {
                 .toList();
 
         part1(numbers);
+        part2(numbers);
+    }
+
+    static void part2(List<Long> numbers) {
+        long total = 0;
+        Map<String, Long> cache = new HashMap<>();
+        for (var num : numbers) {
+            var stones = rec(num, 75, cache);
+            total += stones;
+        }
+        System.err.println(total);
+    }
+
+    static long rec(long num, int blinksRemaining, Map<String, Long> cache) {
+        if (blinksRemaining == 0) return 1;
+
+        String cacheKey = num + "," + blinksRemaining;
+
+        if (cache.containsKey(cacheKey)) return cache.get(cacheKey);
+
+        long result;
+        if (num == 0) {
+            result = rec(1, blinksRemaining - 1, cache);
+        } else {
+            String numString = Long.toString(num);
+            if (numString.length() % 2 == 0) {
+                result = rec(Long.parseLong(numString.substring(numString.length() / 2)),
+                        blinksRemaining - 1, cache);
+                result += rec(Long.parseLong(numString.substring(0, numString.length() / 2)),
+                        blinksRemaining - 1, cache);
+            } else {
+                result = rec(num * 2024, blinksRemaining - 1, cache);
+            }
+        }
+
+        cache.put(cacheKey, result);
+        return result;
     }
 
     static void part1(List<Long> numbers) {
         Deque<Stone> stones = numbers.stream()
-                .map(n -> new Stone(n, 75))
+                .map(n -> new Stone(n, 25))
                 .collect(Collectors.toCollection(() -> new ArrayDeque<>()));
 
         long finalStones = 0;
-        long steps = 0;
         while (!stones.isEmpty()) {
-            System.err.println(stones);
-            if (steps % 10000000 == 0) {
-                System.err.printf("%d: %d (%d)%n", steps, stones.size(), finalStones);
-            }
-            steps++;
 
             var stone = stones.remove();
 
